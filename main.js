4d3373fb0428e634891b0973cfa97f99b6e4bf7a
Keeyaan-tech <78153976+Keeyaan-tech@users.noreply.github.com>
2021-10-09 15:40:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+status = ""; 
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting the objects";
+}
+
+function preload() {
+    img = loadImage('dog_cat.jpg');
+
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    
+    fill("#f54242");
+    text("dog", 45, 75);
+    noFill();
+    stroke("#f54242");
+    rect(30,60,450,350);
+ 
+    fill("#34eb86");
+    text("cat", 320, 120);
+    noFill();
+    stroke("#34eb86");
+    rect(300,90,270,320);
+    
+}
+
+function modelLoaded(){
+console.log("model is loaded");
+status = true;
+objectDetector.detect(img, gotResult);
+
+}
+
+function gotResult(error, results){
+if(error){
+console.log(error);
+}else{
+console.log(results);
+}
+}
